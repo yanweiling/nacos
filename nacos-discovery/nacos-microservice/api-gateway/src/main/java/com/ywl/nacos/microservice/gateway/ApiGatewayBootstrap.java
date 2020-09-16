0890487cc83226073d742a14c106e26192378f1c
yanweiling <yanweilingjob@126.com>
2020-09-16 20:20:55 +0800

    更改

--- nacos-discovery/nacos-microservice/api-gateway/src/main/java/com/ywl/nacos/microservice/gateway/ApiGatewayBootstrap.java
+++ nacos-discovery/nacos-microservice/api-gateway/src/main/java/com/ywl/nacos/microservice/gateway/ApiGatewayBootstrap.java
@@ -3,13 +3,26 @@ package com.ywl.nacos.microservice.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class ApiGatewayBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayBootstrap.class);
     }
+
+    /**
+     * 注解方式配置路由
+     * @param builder
+     * @return
+     */
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
+//        return builder.routes()
+//                .route(p->p.path("/application11/**").uri("lb://application11"))
+//                .build();
+//    }
 }
